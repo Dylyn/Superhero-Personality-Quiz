be69ab477e4c40578bbd6954100f1af9a0302796
Dylyn <dylan8809@gmail.com>
2024-04-13 16:09:48 -0400

    question images

--- Personality Quiz Project/src/pages/quiz.jsx
+++ Personality Quiz Project/src/pages/quiz.jsx
@@ -16,12 +16,24 @@ import ESTJ from "../images/ESTJ.jpg"
 import ESFJ from "../images/ESFJ.jpg"
 import ENFJ from "../images/ENFJ.jpg"
 import ENTJ from "../images/ENTJ.jpg"
+import q1 from "../images/q1.jpg"
+import q2 from "../images/q2.jpg"
+import q3 from "../images/q3.jpg"
+import q4 from "../images/q4.jpg"
+import q5 from "../images/q5.jpg"
+import q6 from "../images/q6.jpg"
+import q7 from "../images/q7.jpg"
+import q8 from "../images/q8.jpg"
+import q9 from "../images/q9.jpg"
+import q10 from "../images/q10.jpg"
+import q11 from "../images/q11.jpg"
+import q12 from "../images/q12.jpg"
+
 
 export default function QuizSection() {
     const navigate = useNavigate();
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [personalityCounts, setPersonalityCounts] = useState(personalityTypes);
-
+    
     const personalityTypes = [
         {
         name: "Introvert",
@@ -65,10 +77,13 @@ export default function QuizSection() {
         },
     ]
 
+    const [personalityCounts, setPersonalityCounts] = useState(personalityTypes);
+
     const questions = [
         {
             id: 1,
             text: "You wake up one day and discover you have superpowers! Do you:",
+            qimage: q1,
             options: [
                 { id: 'a', text: "Keep it a secret and start practicing your powers in your room, trying not to accidentally break anything.", PType: 'I' },
                 { id: 'b', text: "Immediately tell your best friend about your newfound abilities, hoping they won't spill the beans.", PType: 'E' }
@@ -77,6 +92,7 @@ export default function QuizSection() {
         {
             id: 2,
             text: "While practicing your powers, you accidentally blast a hole in your bedroom wall! Do you:",
+            qimage: q2,
             options: [
                 { id: 'a', text: "Quickly cover it up with a poster and pretend nothing happened.", PType: 'P' },
                 { id: 'b', text: "Tell your parents you just made a new enterance to the your superhero hideout", PType: 'J' }
@@ -85,6 +101,7 @@ export default function QuizSection() {
         {
             id: 3,
             text: "You meet up with your best friend and figure out he's your arch-nemesis! Do you:",
+            qimage: q3,
             options: [
                 { id: 'a', text: "Confront them and try to reason with them, hoping to mend your friendship.", PType: 'F' },
                 { id: 'b', text: "Challenge them to a duel of superpowers even if it costs the friendship", PType: 'T' }
@@ -93,6 +110,7 @@ export default function QuizSection() {
         {
             id: 4,
             text: "Your powers accidentally cause chaos in your town's annual parade. Do you:",
+            qimage: q4,
             options: [
                 { id: 'a', text: "Apologize and offer to help clean up the mess.", PType: 'F' },
                 { id: 'b', text: "Use the chnage of scenery and turn the chaos into a crazy party!", PType: 'T' }
@@ -101,6 +119,7 @@ export default function QuizSection() {
         {
             id: 5,
             text: "You're faced with a moral dilemma: use your powers for personal gain or for the greater good. Do you:",
+            qimage: q5,
             options: [
                 { id: 'a', text: "Take a nap and see what your conscience thinks in the morning.", PType: 'P' },
                 { id: 'b', text: "Choose to use your powers responsibly, even if it means sacrificing some of your desires. (take a nap anyway)", PType: 'J' }
@@ -109,6 +128,7 @@ export default function QuizSection() {
         {
             id: 6,
             text: "You awaken in a dense jungle, surrounded by towering trees and strange creatures. How do you react with your superpowers?",
+            qimage: q6,
             options: [
                 { id: "a", text: "Use your super strength to push through the dense foliage and explore deeper into the jungle.", PType: "S" },
                 { id: "b", text: "Activate your telepathic abilities to communicate with the creatures and seek guidance on how to navigate the jungle.", PType: "N" }
@@ -117,6 +137,7 @@ export default function QuizSection() {
         {
             id: 7,
             text: "A naked old man emerges from the shadows, offering to teach you how to control your superpowers. Do you:",
+            qimage: q7,
             options: [
                 { id: "a", text: "Accept the offer, eager to hone your abilities and unlock your full potential.", PType: "E" },
                 { id: "b", text: "Politely decline, feeling unsure about trusting strangers in this mysterious jungle.", PType: "I" }
@@ -125,6 +146,7 @@ export default function QuizSection() {
         {
             id: 8,
             text: "While training, you encounter a group of hostile yet afraid creatures. How do you handle the situation?",
+            qimage: q8,
             options: [
                 { id: "a", text: "Absolutely annihilate them out of existence with your fists.", PType: "T" },
                 { id: "b", text: "Read their minds to find a peaceful resolution and possibly finding a new ally that could help you figure out where you are", PType: "F" }
@@ -133,6 +155,7 @@ export default function QuizSection() {
         {
             id: 9,
             text: "You come across an ancient temple shrouded in the depths of the jungle. How do you approach it?",
+            qimage: q9,
             options: [
                 { id: "a", text: "Harness your powers to force the door open, eager to explore the secrets hidden within.", PType: "J" },
                 { id: "b", text: "Rely on your supernatural abilities to scout ahead, ensuring safety before venturing inside.", PType: "P" }
@@ -141,6 +164,7 @@ export default function QuizSection() {
         {
             id: 10,
             text: "As you delve into the temple's chambers, you trigger a series of traps! How do you navigate them with your superpowers?",
+            qimage: q10,
             options: [
                 { id: "a", text: "Analyze the traps with your enhanced intellect and use your telekinetic abilities to safely disarm them.", PType: "T" },
                 { id: "b", text: "Rely on your heightened reflexes and agility to swiftly dodge the traps and continue exploring.", PType: "S" }
@@ -149,6 +173,7 @@ export default function QuizSection() {
         {
             id: 11,
             text: "Feeling drained from using your superpowers, you decide to take a moment to recharge. What do you do?",
+            qimage: q11,
             options: [
                 { id: "a", text: "Meditate to channel your energy, focusing on restoring balance within yourself until you fall asleep.", PType: "N" },
                 { id: "b", text: "Battle through it! I will fight through the exhaustion even if it means i pass out! (you pass out)", PType: "E" }
@@ -157,6 +182,7 @@ export default function QuizSection() {
         {
             id: 12,
             text: "You suddenly wake up in your bed, realizing it was all just a vivid dream. How do you feel about your adventure with superpowers?",
+            qimage: q12,
             options: [
                 { id: "a", text: "Be sad wishing your powers were real, but eager to explore the real world again.", PType: "P" },
                 { id: "b", text: "Appreciate the thrilling journey, but glad to return to comforts of reality.", PType: "J" }
@@ -243,6 +269,7 @@ export default function QuizSection() {
         <div key={question.id}>
         <div>{question.id}/12</div>
         <h3>{question.text}</h3>
+        <img src={question.qimage} height={400} width={400} style={{ border: '7px solid black', marginTop: '20px', marginBottom: '20px' }}/>
         {question.options.map(option => (
             <div key={option.id}>
             <button
